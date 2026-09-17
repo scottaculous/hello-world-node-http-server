4573cd74fa5b76582cebe5d5a807ec1f1959ca9e
scottaculous <scottaculous@gmail.com>
2026-09-17 17:23:49 -0400

    Add ko-pour.yaml and some chnges to deploy the app

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync(__dirname + '/index.html', 'utf8');
-var favicon = fs.readFileSync(__dirname + '/favicon.ico');
-const get_hash = require('./hash.js');
-console.log('cwd', __dirname);
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+
+const host = '0.0.0.0';
+const port = Number(process.env.PORT) || 3000;
+const index = fs.readFileSync(__dirname + '/index.html', 'utf8');
+const favicon = fs.readFileSync(__dirname + '/favicon.ico');
 
 http.createServer(function (req, res) {
-  console.log("URL:", req.url);
+  if (req.url === '/healthz') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('ok');
+    return;
+  }
+
   if (req.url.indexOf('favicon') > -1) {
     res.writeHead(200, {'Content-Type': 'image/x-icon'});
     res.end(favicon);
+    return;
   }
-  else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    get_hash(function(e, GIT_COMMIT_HASH, stder) {
-      console.log(e, GIT_COMMIT_HASH, stder);
-      res.end(index.replace(/{GIT_COMMIT_HASH}/g, GIT_COMMIT_HASH)); // replace('{GIT_COMMIT_HASH}', );
-    })
 
-  }
-}).listen(process.env.PORT || 3000);
+  const revision = process.env.K_REVISION || 'local';
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index.replace(/{GIT_COMMIT_HASH}/g, revision));
+}).listen(port, host, function () {
+  console.log(`Listening on http://${host}:${port}`);
+});
